0485b699ceeee432ffed06e27a90e10ee16c03eb
vasko <vasko@childish.eu>
2023-08-12 13:29:22 +0300

    Combined assets in a job and set schedule

--- my-dagster/my_dagster/__init__.py
+++ my-dagster/my_dagster/__init__.py
@@ -1,9 +1,22 @@
-from dagster import Definitions, load_assets_from_modules
+from dagster import (
+    Definitions,
+    AssetSelection,
+    ScheduleDefinition,
+    load_assets_from_modules,
+    define_asset_job,
+)
 
 from . import assets
 
 all_assets = load_assets_from_modules([assets])
 
+parking_violations_job = define_asset_job("parking_violations_job", selection=AssetSelection.all())
+parking_violations_schedule = ScheduleDefinition(
+    job=parking_violations_job,
+    cron_schedule='0 15 * * *',
+)
+
 defs = Definitions(
     assets=all_assets,
+    schedules=[parking_violations_schedule],
 )
